35c052696f80b09a59d1d3a919bc2658aa406692
trainsterlie <smhmyheadlmfao@gmail.com>
2026-06-16 22:39:44 +0800

    Update stepper_RA_v2.ino

--- stepper_RA_movement/stepper_RA_v2/stepper_RA_v2.ino
+++ stepper_RA_movement/stepper_RA_v2/stepper_RA_v2.ino
@@ -1,7 +1,8 @@
 #include <TMC2209.h>
 #include <SoftwareSerial.h>
 #define RATIO 150
-#define STEPS 8 //must be in 2^n form, eg 2,4,8,16,64,128,256
+#define STEPS 256 //must be in 2^n form, eg 2,4,8,16,64,128,256
+#define SLEW_SPEED 12000
 // SoftwareSerial can be used on Arduino boards without HardwareSerial ports,
 // such as the Uno, Nano, and Mini.
 //
@@ -17,13 +18,13 @@ const uint8_t EN_PIN = 3;
 const uint8_t STEP_PIN = 6;
 const uint8_t DIR_PIN = 5;
 const int POT_PIN = A0;
-const uint8_t BUT_PIN = 9;
+const uint8_t BUT_REV_PIN = 9;
+const uint8_t BUT_SLEW_PIN = 12;
 SoftwareSerial soft_serial(RX_PIN, TX_PIN);
-const int32_t RUN_VELOCITY = 1000;
+const int32_t RUN_VELOCITY = 249; //249
+
 const int32_t STOP_VELOCITY = 0;
 int pot_value;
-int but_value;
-bool pressed;
 bool inversed = true;
 const double time_between_pulses = (((86164.0905/360)*0.9*RATIO)/STEPS)*1000000; //in microseconds
 // current values may need to be reduced to prevent overheating depending on
@@ -35,7 +36,8 @@ void setup()
 {
   pinMode(DIR_PIN, OUTPUT);
   pinMode(STEP_PIN, OUTPUT);
-  pinMode(BUT_PIN, INPUT_PULLUP);
+  pinMode(BUT_REV_PIN, INPUT_PULLUP);
+  pinMode(BUT_SLEW_PIN, INPUT_PULLUP);
   digitalWrite(DIR_PIN, LOW);
   digitalWrite(STEP_PIN, LOW);
   Serial.begin(9600);
@@ -43,10 +45,11 @@ void setup()
   stepper_driver.setup(soft_serial, 9600);
   Serial.println(stepper_driver.isCommunicating());
   stepper_driver.setMicrostepsPerStep(STEPS);
+  //stepper_driver.disableCoolStep();
   stepper_driver.enableAutomaticCurrentScaling();
   stepper_driver.enableAutomaticGradientAdaptation();
   stepper_driver.enableInverseMotorDirection();
-  //stepper_driver.setRMSCurrent(400, 0.22); 
+  stepper_driver.setRMSCurrent(200, 0.11); 
   stepper_driver.enable();
   stepper_driver.moveAtVelocity(RUN_VELOCITY);
 }
@@ -56,30 +59,48 @@ void Pulse(const uint8_t step){
   digitalWrite(STEP_PIN, HIGH);
   digitalWrite(STEP_PIN, LOW);
 }
-void loop()
-{
-  but_value = digitalRead(BUT_PIN);
+bool ButtonPress(const uint8_t PIN){
+  int but_value = digitalRead(PIN);
+  bool pressed = false;
   if (but_value == LOW){
     pressed = true;
     Serial.println("Pressed");
-    if (inversed){
-    stepper_driver.disableInverseMotorDirection();
-    inversed = false;
-    }
-    else{
-      stepper_driver.enableInverseMotorDirection();
-      inversed = true;
-    }
-    delay(200);
+    delay(500);
   }
   else{
     pressed = false;
   }
-  pot_value = map(analogRead(POT_PIN), 0, 1023, 0, 800);
-  Serial.println(pot_value);
-  stepper_driver.setRMSCurrent(pot_value, 0.22);
+  return pressed;
+}
+void Slew(const uint32_t SLEW_PIN){
+  int but_value = digitalRead(SLEW_PIN);
+  stepper_driver.moveAtVelocity(SLEW_SPEED);
+  Serial.println("Slewing!");
+  while (digitalRead(SLEW_PIN) == LOW){}
+  stepper_driver.moveAtVelocity(RUN_VELOCITY);
+  Serial.println("Stopped Slewing");
+}
+void loop()
+{
+  if (ButtonPress(BUT_REV_PIN)){
+    if (inversed){
+      stepper_driver.disableInverseMotorDirection();
+      inversed = false;
+    }
+    else{
+      stepper_driver.enableInverseMotorDirection();
+      inversed = true;
+  }
+  }
+  if (ButtonPress(BUT_SLEW_PIN)){
+    Slew(BUT_SLEW_PIN);
+  }
+  }
+  //pot_value = map(analogRead(POT_PIN), 0, 1023, 0, 800);
+  //Serial.println(pot_value);
+  //stepper_driver.setRMSCurrent(pot_value, 0.22);
+  //delay(100);
   //Pulse(STEP_PIN);
   //Serial.println(stepper_driver.isSetupAndCommunicating());
   
 
-}
